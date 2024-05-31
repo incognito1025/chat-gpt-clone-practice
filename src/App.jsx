fe6364d98b981a75033ed3c5146d9cee005f79e6
incognito1025 <nkhanamart@gmail.com>
2024-05-31 19:20:06 -0400

    initial commit and additional items

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex">
+      <div className="hidden md:block md:w-[20%]">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
